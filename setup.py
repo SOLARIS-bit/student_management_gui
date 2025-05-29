76d3dafe8314aa393d63f61eb6e635853f9a4ae0
SOLARIS-bit <shaltonkennedy@gmail.com>
2025-05-29 10:46:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="student_management_gui",
+    version="1.0.0",
+    author="Your Name",
+    description="A colorful, scrollable Tkinter-based GUI to manage students, subjects, and grades.",
+    packages=find_packages(),
+    install_requires=[
+        # tkinter is usually built-in; no need to specify unless using a special environment
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "student-management=main:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
